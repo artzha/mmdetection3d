f1bbbd2adf9fa3db103e5498effefaf050a1e940
artzha <arthurz@utexas.edu>
2023-03-09 14:17:13 -0600

    Added launcher train files for batch models

--- tools/ls6_slurm_create_data.bash
+++ tools/ls6_slurm_create_data.bash
@@ -2,12 +2,18 @@
 #SBATCH -J create_mmlab_data               # Job name
 #SBATCH -o create_mmlab_data.%j            # Name of stdout output file (%j expands to jobId)
 #SBATCH -p normal                          # Queue name
-#SBATCH -N 2                               # Total number of nodes requested (128 cores/node)
+#SBATCH -N 1                               # Total number of nodes requested (128 cores/node)
 #SBATCH -n 2                               # Total number of mpi tasks requested
-#SBATCH -t 00:05:00                        # Run time (hh:mm:ss)
-#SBATCH --reservation Containers_Training  # a reservation only active during the training
+#SBATCH -t 06:00:00                        # Run time (hh:mm:ss)
+#SBATCH -A IRI23004                        # Allocation name
 
-cd $WORK/09156/arthurz/mmdetection3d
+export OMP_NUM_THREADS=2
 module load tacc-apptainer
-ibrun singularity exec mmdetection3d.sif python3 /mmdetection3d/tools/create_data.py waymo --root-path /scratch/09156/arthurz/mmlab/waymo --out-dir /scratch/09156/arthurz/mmlab/waymo --workers 128 --extra-tag waymo
-ibrun singularity exec mmdetection3d.sif python3 /mmdetection3d/tools/create_data.py nuscenes --root-path /scratch/09156/arthurz/mmlab/nuscenes --out-dir /scratch/09156/arthurz/mmlab/nuscenes --extra-tag nuscenes
+module load tacc-singularity
+cd /work/09156/arthurz/mmdetection3d
+
+module load launcher
+export LAUNCHER_WORKDIR=/work/09156/arthurz/mmdetection3d
+export LAUNCHER_JOB_FILE=${LAUNCHER_WORKDIR}/tools/launcher_create_data
+
+${LAUNCHER_DIR}/paramrun
