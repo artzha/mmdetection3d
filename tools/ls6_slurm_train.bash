822168c9194bb91dddfffa4700e01402eba2841e
artzha <arthurz@utexas.edu>
2023-03-12 01:02:34 -0600

    Updated launcher file

--- tools/ls6_slurm_train.bash
+++ tools/ls6_slurm_train.bash
@@ -3,11 +3,11 @@
 #SBATCH -o train_mmlab_model.%j            # Name of stdout output file (%j expands to jobId)
 #SBATCH -p gpu-a100                        # Queue name
 #SBATCH -N 3                               # Total number of nodes requested (128 cores/node)
-#SBATCH -n 1                               # Total number of mpi tasks requested
+#SBATCH -n 9                               # Total number of mpi tasks requested
 #SBATCH -t 00:05:00                        # Run time (hh:mm:ss)
 #SBATCH -A IRI23004                        # Allocation name
 
-export OMP_NUM_THREADS=4
+export OMP_NUM_THREADS=3
 
 module load tacc-apptainer
 module load tacc-singularity
