d7d9583abcb6579688a4346c6ff6f9fe13fe29fb
Arthur K Zhang <arthurz@utexas.edu>
2023-03-13 14:48:45 -0500

    Updated nuscenes pp to eval on kitti

--- configs/pointpillars/hv_pointpillars_secfpn_sbn_2x16_2x_waymo-3d-3class.py
+++ configs/pointpillars/hv_pointpillars_secfpn_sbn_2x16_2x_waymo-3d-3class.py
@@ -1,9 +1,25 @@
+# _base_ = [
+#     '../_base_/models/hv_pointpillars_secfpn_waymo.py',
+#     '../_base_/datasets/waymoD5-3d-3class.py',
+#     '../_base_/schedules/schedule_2x.py',
+#     '../_base_/default_runtime.py',
+# ]
+
+#KITTI EVAL
 _base_ = [
     '../_base_/models/hv_pointpillars_secfpn_waymo.py',
-    '../_base_/datasets/waymoD5-3d-3class.py',
+    '../_base_/datasets/kitti-3d-3class.py',
     '../_base_/schedules/schedule_2x.py',
     '../_base_/default_runtime.py',
 ]
+
+#NUSCENES
+# _base_ = [
+#     '../_base_/models/hv_pointpillars_secfpn_waymo.py',
+#     '../_base_/datasets/nus-3d.py',
+#     '../_base_/schedules/schedule_2x.py',
+#     '../_base_/default_runtime.py',
+# ]
 
 # data settings
 data = dict(train=dict(dataset=dict(load_interval=1)))
